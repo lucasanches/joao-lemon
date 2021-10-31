5fa806b6f229631adbbed16d76a8f68f713e9fa0
Lucas Sanches <lucasdesanches@gmail.com>
2021-10-30 21:24:09 -0300

    preloader - js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
+//Variable
 var btnContact = document.querySelector('.jl-btn-contact');
 
+//Page preloader
+window.addEventListener('load', function () {
+    var pagePreloader = document.querySelector('.jl-preloader');
+    pagePreloader.classList.add('jl-fade-out');
+
+    setTimeout(function () {
+        pagePreloader.style.display = 'none';
+    }, 2000);
+});
+
+//Contact box 
 btnContact.addEventListener('click', function () {
     var boxContact = document.querySelector('.jl-contact-info');
     boxContact.classList.toggle('jl-is-open');
